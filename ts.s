f798983f75117acf3482a5684e4969467b662a40
konsada <keon.sadatian@gmail.com>
2016-02-04 19:22:28 -0800

    MTX4.1

--- ts.s
+++ ts.s
@@ -1,46 +1,45 @@
-# -------------- ts.s file --------------------------- 
-          MTXSEG = 0x1000
+        OSSEG  = 0x1000
 	
-         .globl  _main,_proc,_procSize,_running,_scheduler
-         .global  _tswitch
+       .globl _main,_running,_scheduler,_proc,_procSize  ! IMPORT
+       .globl _tswitch
+	
+        jmpi   start,OSSEG
 
-	
-          jmpi  start, MTXSEG
-start:    mov   ax,cs
-          mov   ds,ax
-          mov   ss,ax
-	
-          mov   sp,#_proc       ! sp->proc
-          add   sp, _procSize   ! sp->high end of proc
-	
-          call  _main
+start:	mov  ax,cs
+	mov  ds,ax
+	mov  ss,ax
 
-dead:	  jmp   dead
+        mov  sp,#_proc
+	add  sp,_procSize
+
+	call _main
+idle:	 jmp  idle
 	
 _tswitch:
-SAVE:	  push  ax
-          push  bx
-          push  cx
-          push  dx
-          push  bp
-          push  si
-          push  di
-	  pushf
-	  mov   bx,_running
- 	  mov   2[bx], sp
+SAVE:	
+	push ax
+	push bx
+	push cx
+	push dx
+	push bp
+	push si
+	push di
+	pushf
+	mov  bx,_running
+	mov  2[bx],sp
 
-NEXT_RUN: call	_scheduler
+FIND:	call _scheduler
 
-_resume:
-RESUME:   mov	bx,_running
-	  mov	sp, 2[bx]
-	  popf
-	  pop   di
-          pop   si
-          pop   bp
-          pop   dx
-          pop   cx
-          pop   bx
-          pop   ax
+RESUME:
+	mov  bx,_running
+	mov  sp,2[bx]
+	popf
+	pop  di
+	pop  si
+	pop  bp
+	pop  dx
+	pop  cx
+	pop  bx
+	pop  ax
 
-	  ret
+	ret
